8975fef7b5eb15d4ed0e167d00542d0fec38e0d7
Rodrigo CG <colqueariel023@gmail.com>
2024-03-16 20:35:32 -0400

    feat: add class Category

---
+++ src/main/java/com/trivia/trivia/entities/Category.java
@@ -0,0 +1,10 @@
+package com.trivia.trivia.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Category {
+    private String name;
+    private String description;
+}
