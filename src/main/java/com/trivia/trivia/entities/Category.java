416a9c816e4682cbf93394f23fd4f3883d8e77f2
Rodrigo CG <colqueariel023@gmail.com>
2024-03-18 09:55:45 -0400

    feat: add annotation and JPA interface to Category class

--- src/main/java/com/trivia/trivia/entities/Category.java
+++ src/main/java/com/trivia/trivia/entities/Category.java
@@ -1,10 +1,19 @@
 package com.trivia.trivia.entities;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.Data;
 
-@Getter @Setter
-public class Category {
+
+@Data
+@Entity
+@Table(name = "category")
+public class Category{
+    @Id
+    // auto increment
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "description")
     private String description;
 }
